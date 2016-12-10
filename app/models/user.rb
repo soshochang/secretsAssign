b4dcab9eea857c38c2ecebfbbc1528860b64b172
Sosho Chang <soshochang@gmail.com>
2016-12-10 11:55:47 -0800

    User assign

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :name, presence: true
+  validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }
+end
