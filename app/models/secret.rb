7ba299cf088de3eb623f46fca3bc450c03fb2703
Sosho Chang <soshochang@gmail.com>
2016-12-10 16:53:17 -0800

    secrets

---
+++ app/models/secret.rb
@@ -0,0 +1,5 @@
+class Secret < ApplicationRecord
+  belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :users_liked, through: :likes, source: :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :secrets
+  has_many :likes, dependent: :destroy
+  has_many :secrets_liked, through: :likes, source: :secret
 
   email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :name, presence: true
